d50434d649c32b91f4fd8a97dcb74c64d6ff55fc
fareedzafar657 <fareedzafar657@gmail.com>
2025-05-24 13:34:10 +0500

    image height auto

--- components/character-image.tsx
+++ components/character-image.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 const CharacterImage = () => {
   return (
     <div className='absolute right-0 -bottom-[5px] lg:relative '>
-        <Image src={"/naruto.png"} width={350} height={700} alt='shadow' className='w-[300px] lg:w-[400px] h-full brightness-0 opacity-40 translate-x-[10px] translate-y-[10px] z-0' />
-        <Image src={"/naruto.png"} width={350} height={700} alt='character' className='absolute inset-0 w-[300px] lg:w-[400px] h-full z-10' />
+        <Image src={"/naruto.png"} width={350} height={700} alt='shadow' className='w-[300px] lg:w-[400px] h-auto brightness-0 opacity-40 translate-x-[10px] translate-y-[10px] z-0' />
+        <Image src={"/naruto.png"} width={350} height={700} alt='character' className='absolute inset-0 w-[300px] lg:w-[400px] h-auto z-10' />
     </div>
   )
 }
